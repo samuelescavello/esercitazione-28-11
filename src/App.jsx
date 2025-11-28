2eb6611417a871352eb915afa4a751b98b596332
samuelescavello <scavellosamuele23@gmail.com>
2025-11-28 15:14:16 +0100

    button login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
-  return <></>;
+  const [user, setUser] = useState(null);
+  const utente = {
+    name: "Mario Rossi",
+    age: 30,
+    email: "mario.rossi@example.com",
+    status: "premium",
+  };
+  function onLogin() {
+    setUser(utente);
+  }
+  return (
+    <>
+      {user ? (
+        <div>utente loggato</div>
+      ) : (
+        <button onClick={onLogin}>login</button>
+      )}
+    </>
+  );
 }
 
 export default App;
